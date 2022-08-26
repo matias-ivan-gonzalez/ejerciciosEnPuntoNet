44b8d88d6e4a5228da7a2020d5c6462a4393ada0
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-26 01:05:34 -0300

    envio punto 20

---
+++ practica1/punto20/Program.cs
@@ -0,0 +1,16 @@
+﻿/*¿Cuál es el problema del código siguiente y cómo se soluciona?
+int i = 0;
+for (int i = 1; i <= 10;)
+{
+Console.WriteLine(i++);
+}
+*/
+
+/* El problema es la doble declaración de i, se soluciona quitando el segundo int
+   en el for o sacando la primera asignacion(pero dejando el segundo)
+   */
+for (int i = 1; i <= 10;)
+{
+Console.WriteLine(i++);
+}
+Console.ReadLine();
