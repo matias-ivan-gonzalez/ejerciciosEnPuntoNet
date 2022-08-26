79aa24b9445fb3d1cdff266fe9f4306c55151c89
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-26 00:53:00 -0300

    envio punto 17

---
+++ practica1/punto17/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Utilizar el operador ternario condicional para establecer el contenido de una variable entera con
+el menor valor de otras dos variables enteras.
+*/
+
+int a=10,b=5;
+Console.WriteLine($"a={a}");
+Console.WriteLine($"b={b}");
+int c = (a<b) ? a : b;
+Console.WriteLine($"c={c}");
+Console.ReadLine();
