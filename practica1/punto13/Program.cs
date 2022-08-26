d226fce8630038416e4eaa4fc37c272a34776cbd
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-26 00:14:26 -0300

    envio punto 13

---
+++ practica1/punto13/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Escribir un programa que calcule la suma de dos números reales introducidos por teclado y
+muestre el resultado en la consola (utilizar double.Parse(st) para obtener el valor double a
+partir del string st.
+*/
+Console.Write("Ingrese un número real: ");
+double numero1 = double.Parse(Console.ReadLine());
+Console.Write("Ingrese otro número real: ");
+double numero2 = double.Parse(Console.ReadLine());
+Console.WriteLine(numero1 + numero2);
+Console.ReadLine();
