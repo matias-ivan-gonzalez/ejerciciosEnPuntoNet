c33486323723107d91e4e130a0160e5b57aa5b75
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-25 22:45:40 -0300

    envio punto 6

---
+++ practica1/punto6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Ingrese una palabra:");
+String entrada = Console.ReadLine();
+while(entrada!=""){
+    Console.WriteLine("La palabra ingresada tiene " + entrada.Length);
+    Console.Write("Ingrese una palabra:");
+    entrada = Console.ReadLine();
+}
+Console.WriteLine("Terminó la ejecución");
+Console.ReadLine();
