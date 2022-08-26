2221010c08bb7a1be5f751eea3b96836aed82140
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-25 23:41:56 -0300

    envio punto 10

---
+++ practica1/punto10/Program.cs
@@ -0,0 +1,5 @@
+﻿for (int i = 1 ; i <= 1000 ; i++) {
+    bool resultado = (i % 17 == 0 || i % 29 == 0) ? true : false;
+    Console.Write(resultado==true ? i + "\n" : null);
+}
+Console.ReadLine();
