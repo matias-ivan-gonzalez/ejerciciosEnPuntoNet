2481baa12e3c42e68c062f6c056aad3fd9987de5
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-26 18:20:33 -0300

    envio de punto 1 y 2

---
+++ practica2/punto1/Program.cs
@@ -0,0 +1,6 @@
+﻿char c1 = 'A';
+string st1 = "A";
+object o1 = c1; // boxing
+object o2 = st1; // boxing
+char c2 = (char)o1; // unboxing
+string st2 = (string)o2; //unboxing
---
+++ practica2/punto2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Sea el siguiente código:
+
+El tipo object es un tipo referencia, por lo tanto luego de la sentencia o2 = o1 ambas
+variables están apuntando a la misma dirección. ¿Cómo explica entonces que el resultado en la
+consola no sea “Z Z”?
+*/
+
+object o1 = "A";
+object o2 = o1;
+o2 = "Z";
+Console.WriteLine(o1 + " " + o2);
+
+Console.ReadLine();
+
+/*
+    Respuesta:
+    No es Z Z porque luego de que hace o2 = o1, o2 guarda la referencia de o1,
+    pero luego cambia por una Z, siguiendo o1 teniendo el mismo estado que antes
+    (una "A"), quedando al final A Z
+*/
