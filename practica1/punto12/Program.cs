4cb4ec7b5873f12ec59fc69487a8316644615bda
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-26 00:03:35 -0300

    envio punto 12

---
+++ practica1/punto12/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Escribir un programa que imprima todos los divisores de un número entero ingresado desde la
+consola.
+*/
+Console.Write("Ingrese un número entero:");
+int numero = int.Parse(Console.ReadLine());
+for (int i = 1; i <= numero; i++) {
+    Console.Write(
+        (numero % i == 0) ? i + "\n" : null
+    );
+}
+Console.ReadLine();
