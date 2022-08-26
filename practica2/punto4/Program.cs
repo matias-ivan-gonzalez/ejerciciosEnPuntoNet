d8e31f25ad0873feaa6d2bfbd214cb76b974b5bc
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-26 19:27:39 -0300

    envio punto 4

---
+++ practica2/punto4/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+¿Cuál es la salida por consola si no se pasan argumentos por la línea de comandos?
+*/
+
+Console.WriteLine(args == null); // false
+Console.WriteLine(args.Length); // 0
+
+Console.ReadLine();
