dab5356ec2d55ade59a6147e4260dd774be1ceee
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-25 22:57:23 -0300

    envio punto 8

---
+++ practica1/punto8/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+Sea st una variable de tipo string correctamente declarada. ¿Es válida la siguiente instrucción:
+Console.WriteLine(st=Console.ReadLine());?
+*/
+String st;
+Console.WriteLine(st=Console.ReadLine());
+Console.ReadLine();
+//Si, es válida
