09a986c919e02fb63f9e5c615865c077ea2ec0a9
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-26 19:15:30 -0300

    envio punto 3

---
+++ practica2/punto3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Analizar la siguiente porción de código para calcular la sumatoria de 1 a 10. ¿Cuál es el error?
+¿Qué hace realmente?
+*/
+int sum = 0;
+Console.WriteLine(sum);
+int i = 1;
+while (i <= 10)
+{
+    sum += i++;
+    Console.WriteLine(sum);
+}
+Console.ReadLine();
+
+// El error era que tenia un ; luego del while(i <= 10)
