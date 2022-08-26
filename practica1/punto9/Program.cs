04d9c70a00c732d7e3dd314e6daa91b4e07220a2
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-25 23:21:58 -0300

    envio punto 9

---
+++ practica1/punto9/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Ingrese una palabra: ");
+String palabra1 = new String(Console.ReadLine().OrderBy(ch => ch).ToArray());
+Console.Write("Ingrese otra palabra: ");
+String palabra2 = new String(Console.ReadLine().OrderBy(ch => ch).ToArray());
+Console.WriteLine(
+    (palabra1==palabra2) ? "Simetrica" : "No simetrica"
+);
+Console.ReadLine();
