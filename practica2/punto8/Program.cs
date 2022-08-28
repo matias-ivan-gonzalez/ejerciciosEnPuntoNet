6d711cd38e6606942f45bb65d8d07503217c62a1
Matias Gonzalez <MatiasG2001@outlook.com>
2022-08-28 02:04:58 -0300

    envio de puntos practicos

---
+++ practica2/punto8/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Escribir un programa que reciba una lista de nombres como parámetro
+e imprima por consola un
+saludo personalizado para cada uno de ellos.
+a) utilizando la sentencia for
+b) utilizando la sentencia foreach
+*/
+
+Console.WriteLine("Resolucion con FOR:");
+for (int i = 0; i < args.Length; i++) {
+    Console.WriteLine("Hola {0}",args[i]);
+}
+Console.WriteLine("Resolucion con FOREACH:");
+foreach (var arg in args){
+    Console.WriteLine($"Hola {arg}");
+}
+Console.ReadLine();
