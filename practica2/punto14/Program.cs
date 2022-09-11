e296099ba45e9e8b3e2c656c67a60a9f44862a59
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-11 17:13:22 -0300

    envio punto 14 y 15

---
+++ practica2/punto14/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Implementar un programa que muestre todos los números primos
+entre 1 y un número natural dado (pasado al programa como argumento
+por la línea de comandos). Definir el método bool EsPrimo(int n)
+que devuelve true sólo si n es primo. Esta función debe comprobar si n es
+divisible por algún número entero entre 2 y la raíz cuadrada de n. 
+(Nota: Math.Sqrt(d) devuelve la raíz cuadrada de d)
+*/
+
+// Consultar como hacerlo con el profesor
+Console.Write("Ingrese un número natural: ");
+int numero = int.Parse(Console.ReadLine());
+for (int i = 2; i <= numero; i++) {
+    if(EsPrimo(i)){
+        Console.WriteLine(i + " es primo");
+    }
+}
+Console.ReadLine();
+
+bool EsPrimo(int n){
+    bool resultado = false;
+    if ((n % 2 != 0) && (Math.Sqrt(n) % 2 != 0)){ //si el nro es par y tiene una raíz cuadrada par entonces es primo
+        resultado = true;
+    }
+    return resultado;
+}
---
+++ practica2/punto15/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Escribir una función (método int Fib(int n)) que calcule el término
+n de la serie de Fibonacci.
+Fib(n) = 1, si n <=2
+Fib(n) = Fib(n-1) + Fib(n-2), si n > 2
+*/
+
+Console.Write(Fib(int.Parse(args[0])));
+
+int Fib(int n){
+    return (n==0) ? 0 : ((n<=2) ? 1 : (Fib(n-1) + Fib(n-2)));
+}
