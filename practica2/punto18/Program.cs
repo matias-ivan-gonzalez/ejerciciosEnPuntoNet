eed146d051bd43910177acb4bb918d4e95715404
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-12 23:41:49 -0300

    envio punto 18

---
+++ practica2/punto18/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Codificar el método Swap que recibe 2 parámetros enteros
+e intercambia sus valores. El cambio debe apreciarse en
+el método invocador.
+*/
+
+void Swap(int a,int b){
+    Console.WriteLine("A: " + a);
+    Console.WriteLine("B: " + b);
+    int auxiliar = a;
+    a = b;
+    b = auxiliar;
+    Console.WriteLine("A: " + a);
+    Console.WriteLine("B: " + b);
+}
+
+int primero = int.Parse(args[0]);
+int segundo = int.Parse(args[1]);
+Swap(primero, segundo);
