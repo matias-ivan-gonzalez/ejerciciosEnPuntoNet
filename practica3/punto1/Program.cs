356372920078fd6bb6c690863cf3007e5429f93e
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-13 22:28:11 -0300

    envio punto 1

---
+++ practica3/punto1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Ejecutar y analizar cuidadosamente el siguiente programa:
+*/
+
+Console.CursorVisible = false;
+ConsoleKeyInfo k = Console.ReadKey(false);
+while (k.Key != ConsoleKey.End){
+    Console.Clear();
+    Console.Write($"{k.Modifiers}-{k.Key}-{k.KeyChar}");
+    k = Console.ReadKey(false);
+}
