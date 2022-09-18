4b457792d3b6b7511df6d95e231feb7e5f888f88
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-18 19:46:45 -0300

    envio punto 5

---
+++ practica3/punto5/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Implementar un método que devuelva un arreglo de arreglos con los mismos
+elementos que la matriz pasada como parámetro:
+double[][] GetArregloDeArreglo(double [,] matriz)
+*/
+
+double[][] GetArregloDeArreglo(double [,] matriz){
+    int rows = matriz.GetLength(0);
+    int columns = matriz.GetLength(1);
+    double[][] arregloDeArreglos = new double[rows][];
+    for (int i = 0; i < rows ; i++){
+        arregloDeArreglos[i] = new double[columns];
+        for (int j = 0; j < columns; j++){
+            arregloDeArreglos[i][j] = matriz[i,j];
+            Console.Write(arregloDeArreglos[i][j] + "- ");
+        }
+        Console.WriteLine();
+    }
+    return arregloDeArreglos;
+}
+
+double[,] matriz = new double[,]
+{ {1,2,3,4},
+{5,6,7,8},
+{9,10,11,12} };
+
+GetArregloDeArreglo(matriz);
+Console.ReadLine();
