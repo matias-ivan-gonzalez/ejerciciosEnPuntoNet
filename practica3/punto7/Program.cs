83ced82ded577ba13fa7d99b9d3b5488bf726295
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-18 22:36:32 -0300

    envio punto 7

---
+++ practica3/punto7/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+¿De qué tipo quedan definidas las variables x, y, z en el siguiente código?
+*/
+
+int i = 10;
+var x = i * 1.0; // Queda en double
+var y = 1f; // Queda en float
+var z = i * y; // Queda en float
