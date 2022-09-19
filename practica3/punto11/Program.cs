451a7e68394c3f3ecb19867656d43f2aebbdb49b
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-18 23:42:37 -0300

    envio punto 11

---
+++ practica3/punto11/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Señalar errores de ejecución en el siguiente código
+*/
+
+List<int> a = new List<int>() { 1, 2, 3, 4 };
+a.Remove(5);
+a.RemoveAt(4);
+/*
+El primer metodo bsuca e intenta eliminar el valor 5, pero al no
+encontrarlo, simplemente retorna false, en el caso del removeAt, nos
+da una excepcion ya que intenta acceder al indice número 4 del array
+siendo que este mismo posee un maximo de 3 accesible por indice, y
+dicha peticion excede las dimensiones de la estructura.
+
+*/
