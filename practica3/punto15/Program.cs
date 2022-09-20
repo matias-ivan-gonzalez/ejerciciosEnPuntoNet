0e4d2639b0d35f437170197e1c9171f4202050f6
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-19 21:46:40 -0300

    envio punto 15

---
+++ practica3/punto15/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+¿Qué salida por la consola produce el siguiente código?
+¿Qué se puede inferir respecto de la excepción división por cero en
+relación al tipo de los operandos?
+*/
+
+int x = 0;
+try
+{
+Console.WriteLine(1.0 / x); // IEEE 754, por ello no tira exception
+Console.WriteLine(1 / x); // División entera por cero, tira exception
+Console.WriteLine("todo OK");
+}
+catch (Exception e)
+{
+Console.WriteLine(e.Message);
+}
+
+Console.ReadLine();
