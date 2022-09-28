7897a3304e80a2d6a93f4d0a516c37a4205de503
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-28 20:51:38 -0300

    envio punto 1

--- practica5/punto1/Program.cs
+++ practica5/punto1/Program.cs
@@ -1,4 +1,30 @@
-﻿Cuenta c1 = new Cuenta();
+﻿/*
+Codificar la clase Cuenta de tal forma que el siguiente código produzca la
+salida por consola que se indica.
+
+Se creo la cuenta Id=1
+Se depositó 100 en la cuenta 1 (Saldo=100)
+Se depositó 50 en la cuenta 1 (Saldo=150)
+Se extrajo 120 en la cuenta 1 (Saldo=30)
+Operación denegada - Saldo insuficiente
+Se creo la cuenta Id=2
+Se depositó 200 en la cuenta 2 (Saldo=200)
+Se depositó 800 en la cuenta 2 (Saldo=1000)
+Se creo la cuenta Id=3
+Se depositó 20 en la cuenta 3 (Saldo=20)
+Se extrajo 20 en la cuenta 3 (Saldo=0)
+Se extrajo 1000 en la cuenta 2 (Saldo=0)
+Operación denegada - Saldo insuficiente
+
+DETALLE
+CUENTAS CREADAS: 3
+DEPÓSITOS:       5           - Total depositado:  1170
+Extracciones:    3           - Total extraído:    1140
+                             - Saldo                30
+ * Se denegaron 2 extracciones por falta de fondos
+*/
+
+Cuenta c1 = new Cuenta();
 c1.Depositar(100).Depositar(50).Extraer(120).Extraer(50);
 Cuenta c2 = new Cuenta();
 c2.Depositar(200).Depositar(800);
