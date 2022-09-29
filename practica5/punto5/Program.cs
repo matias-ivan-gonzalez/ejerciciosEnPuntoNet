595463a86b1f90abf19f4fe824ebbd4bd374de2e
Matias Gonzalez <MatiasG2001@outlook.com>
2022-09-28 23:46:27 -0300

    envio punto 5

---
+++ practica5/punto5/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Qué líneas del código siguiente provocan error de compilación? 
+Analizar cuándo es posible acceder a miembros estáticos y de instancia.
+
+
+class A {
+    char c;
+    static string st;
+    void metodo1() {
+        // Este metodo hace un uso correcto de las variables
+        st = "string";
+        c = 'A';
+    }
+    static void metodo2(){
+        new ClaseA().c = 'a'; // Error de compilación, esta clase no existe
+        st = "st2";
+        c = 'B'; // No podemos referenciar a variables de instancia en contextos estaticos
+        new A().st = "otro string"; // Error de compilación
+        // No podemos acceder a la variable estatica st desde una instancia
+    }
+}
+*/
