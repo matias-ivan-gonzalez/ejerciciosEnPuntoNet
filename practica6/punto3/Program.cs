f572c646b48b8c2a5ea1f4835c363d13a6667970
Matias Gonzalez <MatiasG2001@outlook.com>
2022-10-01 17:18:31 -0300

    envio punto 3

---
+++ practica6/punto3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+¿Por qué no funciona el siguiente código?
+¿Cómo se puede solucionar fácilmente?
+
+class Auto {
+    double velocidad;
+    public virtual void Acelerar()
+        => Console.WriteLine("Velocidad = {0}", velocidad += 10);
+}
+class Taxi : Auto {
+    public override void Acelerar()
+    => Console.WriteLine("Velocidad = {0}", velocidad += 5);
+}
+No tiene acceso a la variable velocidad por ser privada, con pasarlo
+a protected se solucionaria
+*/
+
+class Auto {
+    protected double velocidad;
+    public virtual void Acelerar()
+        => Console.WriteLine("Velocidad = {0}", velocidad += 10);
+}
+class Taxi : Auto {
+    public override void Acelerar()
+    => Console.WriteLine("Velocidad = {0}", velocidad += 5);
+}
